3be9c2aa400e8703edcf58ac3001df58dc129d2a
ravitejkallepalli <ravi.kallepalli369@gmail.com>
2019-01-19 10:33:48 -0500

    Abstract data types for Linked List class

--- DataStructure-2019/src/LinkedLists/Main.java
+++ DataStructure-2019/src/LinkedLists/Main.java
@@ -4,12 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		SinglyLinkedList list = new SinglyLinkedList();
-		list.insertNode(20);
-		list.insertNode(30);
-		list.insertNode(40);
+		list.insertAtEnd(20);
 		list.insertAtBeginning(10);
-		list.insertNode(50);
-		list.insertNode(60);
-		list.traverseLinkedList();
+		list.walkThroughNodes();
 	}
 }
--- DataStructure-2019/src/LinkedLists/SinglyLinkedList.java
+++ DataStructure-2019/src/LinkedLists/SinglyLinkedList.java
@@ -2,35 +2,146 @@ package LinkedLists;
 
 public class SinglyLinkedList {
 
-	public Node node;
+	/* ADT - Abstract data types for Linked List class */
+	private Node head;
 
-	public void insertNode(int data) {
-		if (node == null) {
-			node = new Node(data);
+	/*
+	 * Complexity is 0(n) As we have to traverse the end of the list to insert the
+	 * item.
+	 */
+	public void insertAtEnd(int data) {
+		Node node;
+		if (head == null) {
+			head = new Node(data);
 		} else {
-			Node temp = node;
-			while (temp.getNextNode() != null) {
-				temp = temp.getNextNode();
+			node = head;
+			while (node.getNextNode() != null) {
+				node = node.getNextNode();
 			}
-			temp.setNextNode(new Node(data));
+			node.setNextNode(new Node(data));
 		}
-	}
-	
-	public void insertAtBeginning(int data) {
-		Node temp = new Node(data);
-		temp.setNextNode(node);
-		node = temp;
-	}
-	
-	public void deleteAtEnd() {
-		
 	}
 
-	public void traverseLinkedList() {
-		Node temp = node;
-		while (temp != null) {
-			System.out.print(temp.getData() + " ");
-			temp = temp.getNextNode();
+	/*
+	 * Complexity is 0(1) As we don't have to loop through the list, we can replace
+	 * the head with the new node
+	 */
+	public void insertAtBeginning(int data) {
+		Node node = new Node(data);
+		if (head == null) {
+			head = node;
+		} else {
+			node.setNextNode(head);
+			head = node;
 		}
+	}
+
+	public void insertAtPosition(int data, int position) {
+		int count = 1;
+		Node presentNode = head;
+
+		if (position == 0) {
+			insertAtBeginning(data);
+			return;
+		}
+
+		while (count < position && presentNode != null) {
+			presentNode = presentNode.getNextNode();
+			count++;
+		}
+
+		if (count == position) {
+			Node temp = presentNode;
+			Node node = new Node(data);
+			node.setNextNode(temp.getNextNode());
+			temp.setNextNode(node);
+			return;
+		}
+		System.out.println("Unable to insert node at given posit");
+	}
+
+	/*
+	 * Complexity - 0(n)
+	 */
+	public void walkThroughNodes() {
+		Node node = head;
+		while (node != null) {
+			System.out.print(node.getData() + " ");
+			node = node.getNextNode();
+		}
+	}
+
+	public void deleteElementAtBeginning() {
+		Node node = head;
+		if (node == null) {
+			return;
+		} else {
+			head = node.getNextNode();
+			node = null;
+		}
+	}
+
+	public void deleteElemnetAtEnd() {
+		if (head == null) {
+			return;
+		}
+		Node prevNode = head;
+		Node node = head.getNextNode();
+
+		while (node.getNextNode() != null) {
+			prevNode = node;
+			node = node.getNextNode();
+		}
+
+		if (prevNode == head) {
+			head = null;
+		}
+		prevNode.setNextNode(null);
+		node = null;
+	}
+
+	public void deleteElementAtGivenPosition(int position) {
+		if (position == 0) {
+			deleteElementAtBeginning();
+			return;
+		} else if (position == (listLength() - 1)) {
+			deleteElemnetAtEnd();
+			return;
+		} else if (position >= listLength()) {
+			System.out.println("Out of bounds");
+			return;
+		} else {
+			Node prevNode = head;
+			Node node = head.getNextNode();
+			int count = 1;
+			while (count < position) {
+				prevNode = node;
+				node = node.getNextNode();
+				count++;
+			}
+
+			prevNode.setNextNode(node.getNextNode());
+			node = null;
+		}
+	}
+
+	/*
+	 * The complexity of finding the list length is : 0(n) As we have to loop
+	 * through the entire list to find it out.
+	 */
+	public int listLength() {
+		Node node = head;
+		int length = 0;
+		while (node != null) {
+			length++;
+			node = node.getNextNode();
+		}
+
+		System.out.println("\nSize of linked list : " + length);
+		return length;
+	}
+
+	public void clearList() {
+		head = null;
 	}
 }
